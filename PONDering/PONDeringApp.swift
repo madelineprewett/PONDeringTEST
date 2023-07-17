8fb80bd3c0665e6493b845b46a9507660850f634
Madeline Prewett <madsprewett@gmail.com>
2023-07-17 15:19:59 -0700

    Initial Commit

---
+++ PONDering/PONDeringApp.swift
@@ -0,0 +1,17 @@
+//
+//  PONDeringApp.swift
+//  PONDering
+//
+//  Created by scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PONDeringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
