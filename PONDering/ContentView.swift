a5241de97430cd8515d34919c08133540574f0e4
Madeline Prewett <madsprewett@gmail.com>
2023-07-17 15:27:39 -0700

    first commit

--- PONDering/ContentView.swift
+++ PONDering/ContentView.swift
@@ -4,18 +4,15 @@
 //
 //  Created by scholar on 7/17/23.
 //
+// yay
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+
+        Text("PONDering")
+        
     }
 }
 
